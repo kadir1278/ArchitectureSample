6e564bff9b60b7fd5201e0ec84d852d19f03638e
Kadir Ari <kadirdestek1@gmail.com>
2023-10-12 00:35:02 +0300

    update

--- ArchitectureSample/DataAccessLayer/Context/SystemContext.cs
+++ ArchitectureSample/DataAccessLayer/Context/SystemContext.cs
@@ -9,9 +9,11 @@ namespace DataAccessLayer.Context
     public class SystemContext : DbContext
     {
         protected IHttpContextAccessor HttpContextAccessor { get; }
+        private string _domain { get; set; }
         public SystemContext(DbContextOptions dbContextOptions, IHttpContextAccessor httpContextAccessor) : base(dbContextOptions)
         {
             this.HttpContextAccessor = httpContextAccessor;
+            _domain = HttpContextAccessor.HttpContext.Request.Host.ToString();
         }
         public DbSet<User> Users { get; set; }
         public DbSet<ProjectOwner> ProjectOwners { get; set; }
@@ -20,7 +22,15 @@ namespace DataAccessLayer.Context
         {
             modelBuilder.ScriptEncryptAndDecrypt();
 
-            modelBuilder.Entity<User>().HasQueryFilter(x => !x.IsDeleted && x.ProjectOwner.Domain.Contains(HttpContextAccessor.HttpContext.Request.Host.ToString()));
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                if (_domain == "localhost:7081")
+                    entity = entity.HasQueryFilter(e => !e.IsDeleted);
+                else
+                    entity = entity.HasQueryFilter(e => !e.IsDeleted && e.ProjectOwner.Domain == _domain);
+            });
+
             modelBuilder.Entity<ProjectOwner>().HasQueryFilter(x => !x.IsDeleted);
         }
 
